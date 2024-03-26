c5f9693c3f1948fea58ffa8e7fd20c3492afc261
Aditi Shinde <aaditishinde1804@gmail.com>
2024-03-26 20:02:13 +0530

    Splash_Screen w' BottomNav

--- app/src/main/java/com/aditys/threads/screens/Splash.kt
+++ app/src/main/java/com/aditys/threads/screens/Splash.kt
@@ -1,9 +1,11 @@
 package com.aditys.threads.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 
 @Composable
 fun Splash(){
 
+    Text(text = "Splash")
 }
